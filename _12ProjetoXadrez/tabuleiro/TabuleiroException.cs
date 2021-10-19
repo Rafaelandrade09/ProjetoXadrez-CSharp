2b7eecf2587ecae21018d13488cf269c523b943b
Rafael Andrade <rafael.andrade09@outlook.com.br>
2021-10-19 13:12:22 -0300

    Uploading the project

---
+++ _12ProjetoXadrez/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace tabuleiro
+{
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+
+        }
+
+
+
+
+
+
+    }
+}
